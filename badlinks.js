9bdf972934f9b45753e2ef00fc10c5fceb627c2f
Nick Mckay <dominicmckay419@gmail.com>
2019-10-25 17:50:18 -0400

    Added Badliinks
    JS file named BadLinks was added. This adds the functionality of blocking inappropriate links posted in a certain chat.

---
+++ badlinks.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot,message, args) => {
+
+var badLinks = ["www.pornhub.com", "www.xvideos.com", "www.google.com"];
+
+if(message.content = badLinks)
+
+
+
+
+
+
+
+}
